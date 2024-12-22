7738ff8afe7f5ef0a70ed480506a979d3a369a57
Javantax <79591148+Javantax@users.noreply.github.com>
2024-12-22 18:01:12 -0400

    Add html buttons and js querySelector functionality

--- calc.js
+++ calc.js
@@ -1,6 +1,57 @@
-console.log(operate(4, '+' , 4));
+let a;
+let b;
+let o;
 
-function operate (a, o , b) {
+const display = document.querySelector("#display");
+const content = document.createElement("div");
+
+let isSecondOperand = false;
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        const value = btn.textContent;
+
+        if (!isNaN(value)) { // Check if it's a digit
+            if (!isSecondOperand) {
+                a = parseInt(value);
+                isSecondOperand = true;
+
+                console.log(a);
+            } else {
+                b = parseInt(value);
+                
+                console.log(b);
+            }
+        } else if (value === "=") {
+            const result = operate(a, o, b);
+            content.textContent = result;
+            a = result; // Keep result for next calculation
+            b = null;
+            o = '';
+            isSecondOperand = false;
+        } else if (value === "Clear") {
+            content.textContent = '';
+            a = null;
+            b = null;
+            o = '';
+            isSecondOperand = false;
+        } else {
+            o = value;
+            console.log(o);
+        }
+        content.textContent = `${a || ''} ${o || ''} ${b || ''}`;
+    });
+});
+
+display.appendChild(content);
+
+
+
+function operate (a, o , b){
+    // console.log("A value: " + a);
+    // console.log("O value: " + o);
+    // console.log("B value: " + b);
     if (o === '+') {
         return addition(a, b);
     } else if (o === '-') {
@@ -21,7 +72,7 @@ function subtract(a, b) {
 }
 
 function divide(a, b) {
-    return a/b;
+    return b === 0 ? "Error" : a / b;
 }
 
 function multiply(a, b){
